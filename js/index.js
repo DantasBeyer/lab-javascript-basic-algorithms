895e6ab9337ca655dde514a757e94f78b9156cef
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-02-25 23:52:49 +0100

    interation2:conditionals done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 // Iteration 1: Names and Input
 console.log("I,m ready!");
+//driver´s name
+let hacker1 = ["driver"];
+console.log ("The driver´s name is XXXX",);
+
+//navigar´s name
+let hacker2 = ["navigator"];
+console.log("The navigar´s name is YYYY");
 
 // Iteration 2: Conditionals
+
+
+const driver  = "XXXX";
+const navigator ="YYYY";
+if (driver.length > navigator.length){ 
+console.log("The driver has the longest name, it has",driver.length,"characters.");
+} 
+if (driver.length < navigator.length){ 
+  console.log("It seems that the navigator has the longest name, it has",navigator.length,"characters.");
+  } if (driver.length == navigator.length){
+  console.log("Wow, you both have equally long names,",driver.length,"characters!" )
 
 
 // Iteration 3: Loops
