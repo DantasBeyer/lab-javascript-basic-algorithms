5f593805d395d425f2cb409cffaacb7074c9b14a
DantasBeyer <dantasbeyer@hotmail.com>
2024-11-28 12:20:01 +0100

    updated

--- js/index_1.js
+++ js/index_1.js
@@ -1,1 +1,16 @@
 console.log("i'am ready!");
+
+let hacker1 = 'XXXXaa';
+console.log(`The Driver is: ${hacker1}.`);
+let hacker2 = 'YYYYaa';
+console.log(`The Navigator is: ${hacker2}.`);
+
+if ( hacker1.length > hacker2.length){
+  console.log(`The Driver has the longest name, it has ${hacker1.length} characters.`);
+}else if ( hacker1.length < hacker2.length){
+  console.log(`The Navigator has the longest name, it has ${hacker2.length} characters.`);
+}else{
+  console.log(`Wow, you both have equally long names, ${hacker2.length} characters.`);
+}
+
+
